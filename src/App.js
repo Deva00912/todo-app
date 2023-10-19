aaf3d9f6dea293d53c65cee6f4fbf2c1ca57e920
Deva00912 <devashiva8778@gmail.com>
2023-10-19 15:05:47 +0530

    fixed some issues in service worker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./Screens/Login/Login";
 import Register from "./Screens/Register/Register";
